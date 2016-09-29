fc01176d48e7ed0fd0bcc3893e591165e622c671
William Grzybowski <wg@FreeBSD.org>
2016-09-29 13:08:15 -0300

    fix(gui): do not show datasets/zvols comments as inherit
    Ticket:	#14355
    (cherry picked from commit 3e74eb0cac049ec819bc8d5c1d6a01c65ac5dd3e)

--- gui/middleware/notifier.py
+++ gui/middleware/notifier.py
@@ -3958,8 +3958,6 @@ class notifier:
                 data[3] == 'default' or data[3].startswith('inherited')
             ):
                 dval[data[1]] = (data[2], "inherit (%s)" % data[2], 'inherit')
-                if data[1] != 'org.freenas:description':
-                    dval[data[1]] = (data[2], "inherit (%s)" % data[2], 'inherit')
             else:
                 dval[data[1]] = (data[2], data[2], data[3])
         return retval
